0809f1c07fcb2945b2e7a2c180f9c945e5ba24fa
Michael Spencer <sonrisesoftware@gmail.com>
2014-05-15 20:11:32 -0500

    Fixed pyflakes and pep8 warnings

--- tests/autopilot/project_dashboard/test_main.py
+++ tests/autopilot/project_dashboard/test_main.py
@@ -27,24 +27,11 @@ class ProjectsPageTestCase(project_dashboard.ClickAppTestCase):
         self.assertThat(page, NotEquals(None))
         skipButton = page.select_single(objectName='skipButton')
         self.pointing_device.click_object(skipButton)
-        self.wait_until_not_exists(self.main_view,
-            project_dashboard.emulators.InitialWalkthrough)
+        self.wait_until_not_exists(self.main_view, project_dashboard.
+                                   emulators.InitialWalkthrough)
 
     def test_initial_page(self):
-        page = self.main_view.get_projects_page()
         self.assertThat(self.main_view.get_tabs().selectedTabIndex, Equals(0))
-
-    def test_create_project(self):
-        page = self.main_view.get_projects_page()
-        self.assertThat(page.get_projects_count(), Equals(0))
-        
-        button = self.main_view.get_toolbar().click_button('createProject')
-        
-        dialog = self.main_view.get_input_dialog()
-        dialog.enter_text("Sample Project")
-        dialog.ok()
-        
-        self.assertThat(page.get_projects_count, Eventually(Equals(1)))
 
     def test_create_project(self):
         page = self.main_view.get_projects_page()
@@ -59,13 +46,13 @@ class ProjectsPageTestCase(project_dashboard.ClickAppTestCase):
     def create_project(self, name):
         page = self.main_view.get_projects_page()
         count = page.get_projects_count()
-        
-        button = self.main_view.get_toolbar().click_button('createProject')
-        
+
+        self.main_view.get_toolbar().click_button('createProject')
+
         dialog = self.main_view.get_input_dialog()
         dialog.enter_text(name)
         dialog.ok()
-        
+
         self.assertThat(page.get_projects_count, Eventually(Equals(count+1)))
 
     def delete_project(self, index):
@@ -78,13 +65,15 @@ class ProjectsPageTestCase(project_dashboard.ClickAppTestCase):
         self.assertThat(page.get_projects_count, Eventually(Equals(count-1)))
 
     def wait_until_not_exists(self, obj, type='*', **kargs):
-        self.assertThat(lambda: exists(obj, type, **kargs), Eventually(Equals(False)))
+        self.assertThat(lambda: exists(obj, type, **kargs),
+                        Eventually(Equals(False)))
 
     def long_press(self, obj):
         self.pointing_device.move_to_object(obj)
         self.pointing_device.press()
         time.sleep(1)
         self.pointing_device.release()
-    
+
+
 def exists(obj, type='*', **kargs):
     return len(obj.select_many(type, **kargs)) > 0
